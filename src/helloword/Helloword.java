31f1703f605d05380c3ba979435b5010b114ad8b
唐金梁 <tangjinliang@yitong.com.cn>
2019-06-17 15:13:49 +0800

    测试

--- src/helloword/Helloword.java
+++ src/helloword/Helloword.java
@@ -4,7 +4,7 @@ public class Helloword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Word! cs");
+		System.out.println("Hello Word! test");
 	}
 
 }
